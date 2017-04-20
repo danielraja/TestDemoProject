216ddc52fc4edca15679602697488e167ed5eeef
danielraja <danny2mar91@gmail.com>
2017-04-20 12:24:22 +0530

    Demo push

--- TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/DemoClass.java
+++ TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/DemoClass.java
@@ -5,7 +5,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Master updated Change...");
-		System.out.println("Branch updated Change...");
+		System.out.println("Demo updated Change...");
 	}
 
 }
