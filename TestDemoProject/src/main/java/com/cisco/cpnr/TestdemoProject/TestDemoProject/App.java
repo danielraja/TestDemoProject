12be35a57a9e3acbac6a81607b5e331f28da118f
danielraja <danny2mar91@gmail.com>
2017-04-24 12:19:10 +0530

    Demo push

--- TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/App.java
+++ TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/App.java
@@ -4,10 +4,9 @@ package com.cisco.cpnr.TestdemoProject.TestDemoProject;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Diana World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello Diana World!");
+		System.out.println("Added by brnach demo");
+	}
 }
