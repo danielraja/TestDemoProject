70f5d22b625e68e9846e926c3dfb8d234a7d1285
danielraja <danny2mar91@gmail.com>
2017-04-24 12:23:17 +0530

    updated

--- TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/App.java
+++ TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/App.java
@@ -7,6 +7,7 @@ package com.cisco.cpnr.TestdemoProject.TestDemoProject;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello Diana World!");
+		System.out.println("Changed by master");
 		System.out.println("Added by brnach demo");
 	}
 }
