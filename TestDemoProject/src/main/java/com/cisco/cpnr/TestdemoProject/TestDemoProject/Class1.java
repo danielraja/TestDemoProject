019acd4d82bb040634e60497617455d1e1d359b4
danielraja <danny2mar91@gmail.com>
2017-04-24 12:43:02 +0530

    Master

--- TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/Class1.java
+++ TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/Class1.java
@@ -4,5 +4,6 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Master Changes");
 		System.out.println("Branch Demo Changes ");
+		System.out.println("Master rebase Changes ");
 	}
 }
