da952d3822a7c8f0a05550690d6b2a5986259148
danielraja <danny2mar91@gmail.com>
2017-04-24 12:31:09 +0530

    branch changes

--- TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/Class1.java
+++ TestDemoProject/src/main/java/com/cisco/cpnr/TestdemoProject/TestDemoProject/Class1.java
@@ -1,5 +1,8 @@
 package com.cisco.cpnr.TestdemoProject.TestDemoProject;
 
 public class Class1 {
-
+	public static void main(String[] args) {
+		System.out.println("Master Changes");
+		System.out.println("Branch Changes");
+	}
 }
